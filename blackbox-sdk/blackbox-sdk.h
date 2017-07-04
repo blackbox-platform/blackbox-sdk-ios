86a75f62a4253c4c20609249e87094067f2ce87e
Chris Devereux <devereux.chris@gmail.com>
2017-07-04 10:25:55 +0100

    Initial commit

---
+++ blackbox-sdk/blackbox-sdk.h
@@ -0,0 +1,19 @@
+//
+//  blackbox-sdk.h
+//  blackbox-sdk
+//
+//  Created by Christopher Devereux on 03/07/2017.
+//  Copyright © 2017 Redbox Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for blackbox-sdk.
+FOUNDATION_EXPORT double blackbox_sdkVersionNumber;
+
+//! Project version string for blackbox-sdk.
+FOUNDATION_EXPORT const unsigned char blackbox_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <blackbox_sdk/PublicHeader.h>
+
+
