784bc102f8f8ce31b454b7c0548bef98e8e108f3
Chris Devereux <devereux.chris@gmail.com>
2017-07-04 15:54:15 +0100

    Add example integration app

---
+++ Example Integration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example Integration
+//
+//  Created by Christopher Devereux on 04/07/2017.
+//  Copyright © 2017 Redbox Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example Integration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example Integration
+//
+//  Created by Christopher Devereux on 04/07/2017.
+//  Copyright © 2017 Redbox Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
