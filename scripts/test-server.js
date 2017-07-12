067683525fe88e882b5f9fb105a140533c8002e5
Chris Devereux <devereux.chris@gmail.com>
2017-07-12 15:01:36 +0100

    Add request logging to test server

--- scripts/test-server.js
+++ scripts/test-server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
- 
+
 app
-  .use(require('log-request'))
+  .use(require('log-request'), require('body-parser').json(), (req, res) => {
+    if (req.body) {
+      console.log(JSON.stringify(req.body, undefined, 4))
+    }
+
+    res.end();
+  })
   .listen(3000);
 
